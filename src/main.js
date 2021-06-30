c02332bb289c4b1ec766d9faac5ab6daa58cf95b
zxc85166 <jimmya011@gmail.com>
2021-06-30 09:13:30 +0800

    轉換表單

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import { createApp } from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import router from "./router";
 import ElementPlus from "element-plus";
+import VueLoading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "element-plus/lib/theme-chalk/index.css";
 import App from "./App.vue";
 
-createApp(App).use(router).use(ElementPlus).mount("#app");
+createApp(App)
+  .use(router)
+  .use(ElementPlus)
+  .use(VueLoading)
+  .use(VueAxios, axios)
+  .mount("#app");
